eeaa61827ddbf577f4619f2d35ee7881c155a73e
AleksandrBychenko <bychenko.ad@phystech.edu>
2022-10-08 17:57:25 +0300

    example1

---
+++ src/Calculator/Calculator.java
@@ -0,0 +1,34 @@
+package Calculator;
+import java.util.List;
+import java.util.Scanner;
+
+
+
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("Type of input(x sign y):");
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        /*
+        try {
+            String[] operation = s.split(" ");
+        }
+        catch (operation.length != 3 ){
+            print
+
+        }
+        */
+
+
+
+        System.out.println(s);
+        
+        
+        
+
+
+
+
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         File x = new File();
 
+
+
         System.out.println("Hello world! dfdfd" + x.x );
 
     }
