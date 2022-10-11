18b9605e8741056b78958c56cf2707bbe3bd78af
AleksandrBychenko <bychenko.ad@phystech.edu>
2022-10-11 12:09:44 +0300

    example1

--- src/Calculator/Calculator.java
+++ src/Calculator/Calculator.java
@@ -2,6 +2,7 @@ package Calculator;
 import java.util.List;
 import java.util.Scanner;
 
+import static java.lang.System.exit;
 
 
 public class Calculator {
@@ -9,15 +10,37 @@ public class Calculator {
         System.out.println("Type of input(x sign y):");
         Scanner scanner = new Scanner(System.in);
         String s = scanner.nextLine();
-        /*
-        try {
-            String[] operation = s.split(" ");
+        scanner.close();
+        int spaceCount = 0;
+        for (char c : s.toCharArray()) {
+            if (c == ' ') {
+                spaceCount++;
+            }
         }
-        catch (operation.length != 3 ){
-            print
+        String[]  masstr = s.split(" ");
+
+
+
+        if (masstr.length != 3 & spaceCount != 2){
+            System.out.println("Incorrect input format!!!");
+            exit(1);
+        }
+
+        int[] number;
+        number = new int[2];
+
+        try {
+            number[0] = Integer.parseInt (masstr[0]);
+            number[1] = Integer.parseInt (masstr[2]);
 
         }
-        */
+        catch (NumberFormatException nonumbers){
+            //System.out.println(nonumbers.getMessage());
+            System.out.println("You writ no numbers or on the wrong place !!!");
+            exit(1);
+        }
+
+
 
 
 
@@ -31,4 +54,6 @@ public class Calculator {
 
 
     }
+
+
 }
